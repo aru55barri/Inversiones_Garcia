45e4d1b0a7369d29676378c73ea5cbb6e678ddda
aru55barri <fran5barri@gmail.com>
2022-08-14 18:22:32 +0200

    se corrigio parte de usuario en index.php

--- dist/index.php
+++ dist/index.php
@@ -91,7 +91,7 @@
                                             <!----->
                                         </nav>
                                     </div>
-                                    <a class="nav-link" href="index.php">
+                                    <a class="nav-link" href="../src/usuarios.php">
                                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                     Usuario
                                    </a>
