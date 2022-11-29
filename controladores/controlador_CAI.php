75e737b3c15c69eca25b99c49e05baf272f769ee
aru55barri <fran5barri@gmail.com>
2022-11-29 13:44:05 +0100

    segundo avance de CAI

--- controladores/controlador_CAI.php
+++ controladores/controlador_CAI.php
@@ -13,55 +13,69 @@
     class CAIContralador
     {
 
-
-
         //funcion para mostrar
         static public function mostrarCAI()
         {
             global $eliminar;
-        global $modificar;
+            global $modificar;
 
-        $ventas = new ModeloPrincipal();
-        $matriz = $ventas->mostrargeneral("select * from tbl_cai;");
-        $ii = 0;
-        if($matriz != null)
-        {
-           
+            $CAI = new ModeloPrincipal();
+            $matriz = $CAI->mostrargeneral("select * from tbl_cai;");
+            $ii = 0;
+            if($matriz != null)
+            {
+            
 
-            foreach ($matriz as $key => $value)   {
-                foreach ($value as $registro) {?>
-              
-                    <tr>
-                    <td><?=$ii = $ii + 1?></td>
-                    <td><?=$registro['rango_inicial']?></td>
-                    <td><?=$registro['rango_final']?></td>
-                    <td><?=$registro['rango_actual']?></td>
-                    <td><?=$registro['numero_CAI']?></td>
-                    <td><?=$registro['fecha_vencimiento']?></td>
-                    <td><?=$registro['id_usuario']?></td>
-                    <?php
-                        if ($modificar == 1) { ?>
-                            <th><a href="../Login/modificar_CAI.php?id=<?= $registro['id'] ?>" class='btn btn-round btn-info'><i class='fas fa-pen-square'style='color: white'></i></a></th>
-                       <?php }
-                        
-                        if ($eliminar == 1) { ?>
-                            <th><a onclick="eliminar(<?= $registro['id'] ?>)"  class='btn btn-round btn-danger' type='submit'><i class='fas fa-trash-alt' style="color: white;"></i></a></th>
-                        <?php } ?>
-                </tr>
-             <?php   }
-                // Inicio vista en bitacora al mostrar empleados Joel Montoya
-                $modeloPrincipal = new ModeloPrincipal();
-                $fecha = date("Y-m-d-H:i:s");
-                $IDUS = $_SESSION['id_usuario'];
-    
-                $sql = "INSERT INTO tbl_bitacora(id, fecha, id_usuario, id_objeto, accion, descripcion)
-                VALUES(null,'$fecha','$IDUS',22, 'INGRESO','EL USUARIO INGRESA A TABLA CAI')";
-                $modeloPrincipal->insertargeneral($sql);
-                // FIN vista en bitacora al mostrar empleados Joel Montoya
-    
+                foreach ($matriz as $key => $value)   {
+                    foreach ($value as $registro) {?>
+                
+                        <tr>
+                        <td><?=$ii = $ii + 1?></td>
+                        <td><?=$registro['rango_inicial']?></td>
+                        <td><?=$registro['rango_final']?></td>
+                        <td><?=$registro['rango_actual']?></td>
+                        <td><?=$registro['numero_CAI']?></td>
+                        <td><?=$registro['fecha_vencimiento']?></td>
+                        <td><?=$registro['id_usuario']?></td>
+                        <?php
+                            if ($modificar == 1) { ?>
+                                <th><a href="../src/modificar_CAI.php?id=<?= $registro['id'] ?>" class='btn btn-round btn-info'><i class='fas fa-pen-square'style='color: white'></i></a></th>
+                        <?php }
+                            
+                            if ($eliminar == 1) { ?>
+                                <th><a onclick="eliminar(<?= $registro['id'] ?>)"  class='btn btn-round btn-danger' type='submit'><i class='fas fa-trash-alt' style="color: white;"></i></a></th>
+                            <?php } ?>
+                    </tr>
+                <?php   }
+                    // Inicio vista en bitacora al mostrar empleados Joel Montoya
+                    $modeloPrincipal = new ModeloPrincipal();
+                    $fecha = date("Y-m-d-H:i:s");
+                    $IDUS = $_SESSION['id_usuario'];
+        
+                    $sql = "INSERT INTO tbl_bitacora(id, fecha, id_usuario, id_objeto, accion, descripcion)
+                    VALUES(null,'$fecha','$IDUS',22, 'INGRESO','EL USUARIO INGRESA A TABLA CAI')";
+                    $modeloPrincipal->insertargeneral($sql);
+                    // FIN vista en bitacora al mostrar empleados Joel Montoya
+        
+                }
             }
         }
+
+        static function InsertarCAI($rango_inicial, $rango_final, $rango_actual, $numero_CAI, $fecha_vencimiento, $usuario)
+        {
+            include '../Config/conn.php';
+    
+            $modelo = new CAI();
+            $sql = "INSERT INTO tbl_cai (id, rango_inicial, rango_final, rango_actual, numero_CAI, fecha_vencimiento, id_usuario) 
+            VALUES (null,'$rango_inicial', '$rango_final', '$rango_actual', '$numero_CAI','$fecha_vencimiento', '$usuario')";
+            $modelo->insertargeneral($sql);
+
+            $_SESSION['registro'] = 'ok';
+            echo "<script> 
+            location.href ='../src/CAI.php';
+            </script>";
         }
+        
     }
 
     
--- modelos/modelo_CAI.php
+++ modelos/modelo_CAI.php
@@ -6,15 +6,40 @@
 
         private $db;
 
-    public function __construct(){
-        $this->CAI = array();
-        
+
+        public function __construct(){
+            require_once '../Config/Conexion.php';         
+            $this->db = getConexion();
+            $this->Modelo = array();
+        }
+
+        private function setNames(){
+            return $this->db->query("SET NAMES 'utf8'");
+        }
+
+        //insertar datos de una tabla
+        public function insertar($tabla, $data){
+           $consulta="insert into ".$tabla." values(null,". $data .")";
+           $resultado=$this->db->query($consulta);
+           if ($resultado) {
+               return true;
+           }else {
+               return false;
+           }
+       }
+
+       public function insertargeneral($sql){
+            
+        $this->db = getConexion();
+        $resultado=$this->db->query($sql);
+        if ($resultado) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
-    private function setNames(){
-        return $this->db->query("SET NAMES 'utf8'");
+
     }
+?>
 
-    
-
-}
